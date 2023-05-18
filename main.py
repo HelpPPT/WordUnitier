5e6c740b31a432f68ff9e18851708fbdc99c61e9
DongHyeon Kim <hyeoni369@naver.com>
2023-05-19 03:08:03 +0900

    main 수정

--- main.py
+++ main.py
@@ -1,56 +1,30 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import fasttext
-from gensim import models
-from gensim.models import FastText, KeyedVectors
+from gensim.models import FastText
 import pandas as pd
-from typing import Union, List
-from preprocess import Preprocessing
+from typing import List
 from grouping import Grouping
-
-class Text(BaseModel):
-    sentence: str
+from preprocess import preprocess
 
 app = FastAPI()
 
 # CORS 설정 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 모든 도메인 허용
+    allow_origins=['*'],  # 모든 도메인 허용
     allow_credentials=True,
-    allow_methods=["*"],  # 모든 HTTP 메소드 허용
-    allow_headers=["*"],  # 모든 헤더 허용
+    allow_methods=['POST'],  # 모든 HTTP 메소드 허용
+    allow_headers=['*'],  # 모든 헤더 허용
 )
 
-# model = None
 
-# @app.on_event("startup")
-# async def load_model():
-#     global model
-#     model_path = 'cc.ko.300.bin/cc.ko.300.bin'
-#     model = models.fasttext.load_facebook_model(model_path, encoding='euc-kr')
-
-
-
-@app.get('/')
-def hello():
-    return {'hello' : 'world'}
-
-@app.post("/grouping/")
-async def create_file(sentence_list: List[str]):
-    # global model
-    # if model is None:
-    #     return {"message": "모델이 로드되지 않았습니다."}
-
+@app.post('/grouping')
+async def group_words(sentence_list: List[str]):
     data = pd.DataFrame({'document': sentence_list})
-    prep = Preprocessing(data)
-    prep_data = prep.preprocess()
+    prep_data = preprocess(data)
 
     model = FastText(prep_data['tokens'][0], vector_size=10, window=3, min_count=0, workers=4, sg=1)
     grouping = Grouping(prep_data, model)
     result = grouping.group_words()
 
     return result
-
-
